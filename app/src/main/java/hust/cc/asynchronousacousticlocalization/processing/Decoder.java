98ff353239b58494708507453e35d0488284075f
jrnjsyx <981596952@qq.com>
2018-07-19 15:34:21 +0800

    删除了java fft的代码

--- app/src/main/java/hust/cc/asynchronousacousticlocalization/processing/Decoder.java
+++ app/src/main/java/hust/cc/asynchronousacousticlocalization/processing/Decoder.java
@@ -29,52 +29,26 @@ public class Decoder implements FlagVar{
 
     // create variables to store the samples in case frequent new and return
 
-    protected boolean useJni = false;
     public int processBufferSize = 9600;
 
     public int preambleCorrLen = getFFTLen(processBufferSize+LPreamble,LPreamble);
-    public FloatFFT_1D preambleDetectionFFT;
     public int symbolCorrLen = getFFTLen(LSymbol,LSymbol);
-    public FloatFFT_1D symbolDectectionFFT;
-    public float[] preambleCorrResult;
-    public float[] symbolCorrResult;
-    public short[] preambleSamples;
-    public short[] symbolSamples;
-    public float[] preambleFSamples;
-    public float[] symbolFSamples;
-    public float[] graphBuffer;
-    public float[] graphBuffer2;
 
     public float[] upPreambleFFT;
     public float[] downPreambleFFT;
     public float[][] upSymbolFFTs;
     public float[][] downSymbolFFTs;
-    public float[] preambleCorrFitVals;
-    public float[] symbolCorrFitVals;
 
     protected short[] bufferUp;
     protected short[] bufferDown;
 
-    public void initialize(int processBufferSize, boolean useJni){
+    public void initialize(int processBufferSize){
         this.processBufferSize = processBufferSize;
-        this.useJni = useJni;
         bufferedSamples = new short[processBufferSize+beconMessageLength+startBeforeMaxCorr+LPreamble];
         bufferUp = new short[processBufferSize*3];
         bufferDown = new short[processBufferSize*3];
         preambleCorrLen = getFFTLen(processBufferSize+LPreamble+startBeforeMaxCorr,LPreamble);
         symbolCorrLen = getFFTLen(LSymbol,LSymbol);
-        preambleDetectionFFT = new FloatFFT_1D(preambleCorrLen);
-        symbolDectectionFFT = new FloatFFT_1D(symbolCorrLen);
-        preambleCorrResult = new float[preambleCorrLen];
-        symbolCorrResult = new float[symbolCorrLen];
-        preambleSamples = new short[processBufferSize+LPreamble];
-        symbolSamples = new short[LSymbol];
-        preambleFSamples = new float[preambleCorrLen];
-        symbolFSamples = new float[symbolCorrLen];
-        graphBuffer = new float[preambleCorrLen];
-        graphBuffer2 = new float[preambleCorrLen];
-        preambleCorrFitVals = new float[preambleCorrLen];
-        symbolCorrFitVals = new float[symbolCorrLen];
 
         //compute the fft of the preambles and symbols to reduce the computation cost;
         upPreambleFFT = new float[preambleCorrLen];
@@ -86,36 +60,19 @@ public class Decoder implements FlagVar{
             downSymbolFFTs[i] = new float[symbolCorrLen];
         }
 
-        if(!useJni) {
-            System.arraycopy(normalization(upPreamble), 0, upPreambleFFT, 0, upPreamble.length);
-            System.arraycopy(normalization(downPreamble), 0, downPreambleFFT, 0, downPreamble.length);
-            for (int i = 0; i < numberOfSymbols; i++) {
-                System.arraycopy(normalization(upSymbolSamples[i]), 0, upSymbolFFTs[i], 0, upSymbolSamples[i].length);
-                System.arraycopy(normalization(downSymbolSamples[i]), 0, downSymbolFFTs[i], 0, downSymbolSamples[i].length);
-            }
-            getFFT(preambleCorrLen).realForward(upPreambleFFT);
-            getFFT(preambleCorrLen).realForward(downPreambleFFT);
-            for (int i = 0; i < numberOfSymbols; i++) {
-                getFFT(symbolCorrLen).realForward(upSymbolFFTs[i]);
-                getFFT(symbolCorrLen).realForward(downSymbolFFTs[i]);
-            }
-        }else{
-            upPreambleFFT = JniUtils.fft(normalization(upPreamble),preambleCorrLen);
-            downPreambleFFT = JniUtils.fft(normalization(downPreamble),preambleCorrLen);
-            for (int i = 0; i < numberOfSymbols; i++) {
-                upSymbolFFTs[i] = JniUtils.fft(normalization(upSymbolSamples[i]),symbolCorrLen);
-                downSymbolFFTs[i] = JniUtils.fft(normalization(downSymbolSamples[i]),symbolCorrLen);
-            }
-
+        upPreambleFFT = JniUtils.fft(normalization(upPreamble),preambleCorrLen);
+        downPreambleFFT = JniUtils.fft(normalization(downPreamble),preambleCorrLen);
+        for (int i = 0; i < numberOfSymbols; i++) {
+            upSymbolFFTs[i] = JniUtils.fft(normalization(upSymbolSamples[i]),symbolCorrLen);
+            downSymbolFFTs[i] = JniUtils.fft(normalization(downSymbolSamples[i]),symbolCorrLen);
         }
 
+
+
     }
 
 
 
-    public int getProcessBufferSize() {
-        return processBufferSize;
-    }
 
     public short[] bufferedSamples = new short[processBufferSize+beconMessageLength+startBeforeMaxCorr+LPreamble];
 
@@ -153,11 +110,7 @@ public class Decoder implements FlagVar{
         IndexMaxVarInfo indexMaxVarInfo = new IndexMaxVarInfo();
 
         float[] corr;
-        if(!useJni) {
-            corr = xcorr(data1, data2, isFdomain);
-        }else{
-            corr = JniUtils.xcorr(data1,data2);
-        }
+        corr = JniUtils.xcorr(data1,data2);
 
 
         float[] fitVals = getFitValsFromCorr(corr);
@@ -171,61 +124,7 @@ public class Decoder implements FlagVar{
 
 
 
-    public float[] xcorr(float []data1, float[] data2, boolean isFDomain){
 
-        int len = 1;
-        if(!isFDomain) {
-            len = getFFTLen(data1.length,data2.length);
-        }else{
-            len = data1.length;
-        }
-        float[] hData1 = null;
-        float[] hData2 = null;
-        float[] result = getCorrArray(len);
-        FloatFFT_1D fft = getFFT(len);
-
-        if(!isFDomain) {
-            hData1 = new float[len];
-            hData2 = new float[len];
-            System.arraycopy(data1,0,hData1,0,data1.length);
-            System.arraycopy(data2,0,hData2,0,data2.length);
-            fft.realForward(hData1);
-            fft.realForward(hData2);
-//            Log.v("","2 fft time:"+(date2.getTime()-date1.getTime()));
-        }else{
-            hData1 = data1;
-            hData2 = data2;
-        }
-
-        result[0] = hData1[0] * hData2[0]; // value at f=0Hz is real-valued
-        result[1] = hData1[1] * hData2[1]; // value at f=fs/2 is real-valued and packed at index 1
-        for (int i = 1 ; i < result.length / 2 ; ++i) {
-            float a = hData1[2*i];
-            float b = hData1[2*i + 1];
-            float c = hData2[2*i];
-            float d = hData2[2*i + 1];
-
-            result[2*i]     = a*c + b*d;
-            result[2*i + 1] = b*c - a*d;
-        }
-        fft.realInverse(result, true);
-        for(int i=0;i<result.length;i++){
-            result[i] = Math.abs(result[i]);
-        }
-        return result;
-    }
-
-    public float[] getCorrArray(int len){
-        float[] result;
-        if(len == preambleCorrLen) {
-            result = preambleCorrResult;
-        }else if(len == symbolCorrLen){
-            result = symbolCorrResult;
-        }else{
-            result = new float[len];
-        }
-        return result;
-    }
 
     /** corr is the correlation array, chirpLength is the chirp signal's length. return the postion of the max correlation.
      * @auther Ruinan Jin
@@ -278,65 +177,14 @@ public class Decoder implements FlagVar{
 
 
 
-    public float[] xcorr(float s[], int low, int high, float[] reference){
-        float[] s0 = new float[high-low+1];
-        for(int i=low;i<=high;i++){
-            s0[i-low] = s[i];
-        }
-        return xcorr(s0,reference,false);
-    }
-
-
-    public float[] xcorrSignal(short s[], int low, int high, short[] reference){
-        float[] s0 = normalization(s);
-        float[] r0 = normalization(reference);
-        return xcorr(s0,low,high,r0);
-    }
-
-
-    public float[] getData1HalfFFtFromSignals(short[] data1, int data2Len){
-        int len = getFFTLen(data1.length,data2Len);
-
-        FloatFFT_1D fft = getFFT(len);
-
-        float[] hData1 = getFSamples(len);
-        for(int i=0;i<len;i++){
-            if(i<data1.length){
-                hData1[i] = (float) data1[i]/32768;
-            }else{
-                hData1[i] = 0;
-            }
-        }
-
-        fft.realForward(hData1);
-        return hData1;
-    }
-
-    public float[] getFSamples(int len){
-        float[] fSamples;
-        if(len == preambleCorrLen) {
-            fSamples = preambleFSamples;
-        }else if(len == symbolCorrLen){
-            fSamples = symbolFSamples;
-        }else{
-            fSamples = new float[len];
-        }
-        return fSamples;
-    }
 
 
 
-    public FloatFFT_1D getFFT(int len) {
-        FloatFFT_1D fft;
-        if(len == preambleCorrLen) {
-            fft = preambleDetectionFFT;
-        }else if(len == symbolCorrLen){
-            fft = symbolDectectionFFT;
-        }else{
-            fft = new FloatFFT_1D(len);
-        }
-        return fft;
-    }
+
+
+
+
+
 
     public int getFFTLen(int len1, int len2){
         int len = 1;
@@ -347,24 +195,7 @@ public class Decoder implements FlagVar{
     }
 
 
-    public float[] getData1HalfFFtFromSignals(short[] data1, int low, int high, int data2Len){
 
-        short[] data0 = getPreSamples(high-low+1);
-        System.arraycopy(data1,low,data0,0,high-low+1);
-        return getData1HalfFFtFromSignals(data0,data2Len);
-    }
-
-    public short[] getPreSamples(int len){
-        short[] samples;
-        if(len == processBufferSize+LPreamble) {
-            samples = preambleSamples;
-        }else if(len == LSymbol){
-            samples = symbolSamples;
-        }else{
-            samples = new short[len];
-        }
-        return samples;
-    }
 
 
     /**
@@ -433,27 +264,22 @@ public class Decoder implements FlagVar{
         float[] correlationResult = null;
 
         float[] fft;
-        if(!useJni) {
-            fft = getData1HalfFFtFromSignals(s, startIndex, endIndex, upSymbolSamples[0].length);
-        }
-        else{
-            float[] sampleF = normalization(s,startIndex,endIndex);
-            fft = JniUtils.fft(sampleF,2*LSymbol);
-            System.out.println("");
-        }
+        float[] sampleF = normalization(s,startIndex,endIndex);
+        fft = JniUtils.fft(sampleF,2*LSymbol);
+        System.out.println("");
         float max = 0;
         float mean = 0;
         // use the max/mean ratio as the indicator for symbol decoding
         if(isUpSymbol) {
             for (int i = 0; i < numberOfSymbols; i++) {
-                correlationResult = useJni?JniUtils.xcorr(fft,upSymbolFFTs[i]):xcorr(fft,upSymbolFFTs[i],true);
+                correlationResult = JniUtils.xcorr(fft,upSymbolFFTs[i]);
                 max = Algorithm.getMaxInfo(correlationResult, 0, correlationResult.length-1).fitVal;
                 mean = Algorithm.meanValue(correlationResult, 0, correlationResult.length-1);
                 maxRatios[i] = max / mean;
             }
         }else{
             for (int i = 0; i < numberOfSymbols; i++) {
-                correlationResult = useJni?JniUtils.xcorr(fft,downSymbolFFTs[i]):xcorr(fft,downSymbolFFTs[i],true);
+                correlationResult = JniUtils.xcorr(fft,downSymbolFFTs[i]);
                 max = Algorithm.getMaxInfo(correlationResult, 0, correlationResult.length-1).fitVal;
                 mean = Algorithm.meanValue(correlationResult, 0, correlationResult.length-1);
                 maxRatios[i] = max / mean;
