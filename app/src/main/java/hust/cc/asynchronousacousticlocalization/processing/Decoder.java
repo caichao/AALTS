29a3c12279807b107cc9a1adb7579dc2e2207533
jrnjsyx <981596952@qq.com>
2018-07-24 10:40:35 +0800

    改了fitvals

--- app/src/main/java/hust/cc/asynchronousacousticlocalization/processing/Decoder.java
+++ app/src/main/java/hust/cc/asynchronousacousticlocalization/processing/Decoder.java
@@ -196,7 +196,7 @@ public class Decoder implements FlagVar{
 
         }
         for(int i=startBeforeMaxCorr;i<fitVals.length;i++){
-            fitVals[i] = corr[i]*corr[i]*corr[i]*(startBeforeMaxCorr-endBeforeMaxCorr)/fitVals[i];
+            fitVals[i] = corr[i]*(startBeforeMaxCorr-endBeforeMaxCorr)/fitVals[i];
         }
         return fitVals;
     }
@@ -247,7 +247,7 @@ public class Decoder implements FlagVar{
             we mainly detect the preamble by thersholding the value of the fitVals*log(corr+1) in the fit position. it's tested to be the
         best method considering the fitVals and the corr.
          */
-        float fitVal = indexMaxVarInfo.fitVal/corr[indexMaxVarInfo.index]/corr[indexMaxVarInfo.index];
+        float fitVal = indexMaxVarInfo.fitVal;
         float ratio = (float) (fitVal*Math.log(corr[indexMaxVarInfo.index]+1));
         if(/*corr[indexMaxVarInfo.index] > FlagVar.preambleDetectionThreshold*/ fitVal > maxAvgRatioThreshold && ratio > rThreshold && isIndexAvailable(indexMaxVarInfo)) {
             indexMaxVarInfo.isReferenceSignalExist = true;
